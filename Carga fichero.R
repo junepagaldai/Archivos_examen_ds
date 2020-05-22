d74ccaaa53f7c896090a69c55a50a8237ed3d635
june <june.pagaldai@alumni.mondragon.edu>
2020-05-22 09:35:10 +0200

    cambiamos nombre de las columnas

--- Carga fichero.R
+++ Carga fichero.R
@@ -1,2 +1,5 @@
 load("C:/Users/junep/Desktop/Examen_DS/Archivos_examen_ds/muestra32.RData")
 dim(muestra32) #el dataset tiene 337685 observaciones y 3 variables (un código, nombre y el género)
+
+#cambiamos nombre de las columnas
+colnames(muestra32) <- c("nif", "nombre", "genero")
